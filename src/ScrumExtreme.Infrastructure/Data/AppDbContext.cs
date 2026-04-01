38c34475abede0c757b71233bfc9e931de82ddd6
Nordtess <nordtessj@gmail.com>
2026-04-01 14:47:27 +0200

    x (#5)

--- src/ScrumExtreme.Infrastructure/Data/AppDbContext.cs
+++ src/ScrumExtreme.Infrastructure/Data/AppDbContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using ScrumExtreme.Domain.Entities;
 
 namespace ScrumExtreme.Infrastructure.Data;
 
@@ -8,8 +9,7 @@ public class AppDbContext : DbContext
     {
     }
 
-    // Add DbSet properties here as you create domain entities, e.g.:
-    // public DbSet<YourEntity> YourEntities { get; set; }
+    public DbSet<UserStory> UserStories { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
