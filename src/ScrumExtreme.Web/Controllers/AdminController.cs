1983874ff5ca42dcde9cb57bc00f5ccc260c27c6
Nordtess <nordtessj@gmail.com>
2026-04-07 11:16:23 +0200

    x

---
+++ src/ScrumExtreme.Web/Controllers/AdminController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ScrumExtreme.Web.Controllers;
+
+public class AdminController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
