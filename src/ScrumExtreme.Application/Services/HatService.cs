b0f0c9eae9248ca5c9fd8fd1810aab294e343311
fredrikthegel <fredrik.thegel@gmail.com>
2026-04-09 15:02:36 +0200

    Createhatt (#16)
    Klasser för att lägga till lagerförda hattar ( inte klar)

---
+++ src/ScrumExtreme.Application/Services/HatService.cs
@@ -0,0 +1,32 @@
+﻿using ScrumExtreme.Application.Interfaces;
+using ScrumExtreme.Domain.Entities;
+using ScrumExtreme.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScrumExtreme.Application.Services
+{
+    public class HatService
+    {
+        public class UserService : IHatService
+        {
+            private readonly IRepository<Hats> _repository;
+
+            public UserService(IRepository<Hats> repository)
+            {
+                _repository = repository;
+            }
+
+            public async Task<IEnumerable<Hats>> GetAllHatsAsync() =>
+                await _repository.GetAllAsync();
+
+            public async Task CreateUserAsync(Hats hats) =>
+                await _repository.AddAsync(hats);
+
+            public async Task<Hats?> GetByIdAsync(string id) =>
+                await _repository.GetByIdAsync(id);
+        }
+
+    }
+}
---
+++ src/ScrumExtreme.Web/Models/CreateHatsViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ScrumExtreme.Web.Models
+{
+    public class CreateHatsViewModel
+    {
+        [Required(ErrorMessage = "Namn är obligatoriskt.")]
+        public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Storlek är obligatoriskt.")]
+        public string Size { get; set; } = string.Empty;
+
+        [EmailAddress(ErrorMessage = "Pris är obligatoriskt.")]
+        public double Price { get; set; } 
+
+        [Required(ErrorMessage = "Materiallista är obligatoriskt.")]
+        public string MaterialList { get; set; } = string.Empty;
+
+        
+    }
+}
