f063675ccb8ef2ab9e35472250521f4652fc7693
Nordtess <nordtessj@gmail.com>
2026-04-13 10:38:19 +0200

    x

--- src/ScrumExtreme.Web/Program.cs
+++ src/ScrumExtreme.Web/Program.cs
@@ -40,7 +40,7 @@ app.UseAuthorization();
 app.MapStaticAssets();
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Login}/{action=Index}/{id?}")
+    pattern: "{controller=AddCustomer}/{action=Index}/{id?}")
     .WithStaticAssets();
 
 // Ping MongoDB on startup to confirm connection
