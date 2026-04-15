729fb5eb3aeee4cfc85ce747ffc8b37025decbcf
fredrikthegel <fredrik.thegel@gmail.com>
2026-04-15 20:33:43 +0200

    Specialhattar
    Börjat på att fixa specialhattar, lagt till fält i hats med beskrivning samt skapat ny controller och cshtml för det, ändrat css så det fixar textarea

---
+++ src/ScrumExtreme.Web/Controllers/CreateSpecialHatsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ScrumExtreme.Web.Models;
+
+namespace ScrumExtreme.Web.Controllers
+{
+    public class CreateSpecialHatsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new CreateHatsViewModel());
+        }
+    }
+}
