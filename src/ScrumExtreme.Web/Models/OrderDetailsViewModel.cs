573bea8b68aa5dc20b5c78ada3cea9674d05fcf3
LinneaLex <linnea.lekter@gmail.com>
2026-04-19 16:59:45 +0200

    Vy och viewmodel och metod
    Skapat view och viewmodel för orderdetaljer, samt en  metod i ordercontrollern för att ta fram datan om ordern som man vill öppna.

---
+++ src/ScrumExtreme.Web/Models/OrderDetailsViewModel.cs
@@ -0,0 +1,10 @@
+﻿using ScrumExtreme.Domain.Entities;
+
+namespace ScrumExtreme.Web.Models
+{
+    public class OrderDetailsViewModel
+    {
+        public Order Order { get; set; }
+        public string CustomerEmail { get; set; }
+    }
+}
