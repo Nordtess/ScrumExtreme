fb4ba678fa906dfa320b0f0a7c8eefdeb4860151
joelzema <g5joel@hotmail.se>
2026-04-20 21:30:00 +0200

    lägg till material (#45)

--- src/ScrumExtreme.Web/Controllers/AddMaterialController.cs
+++ src/ScrumExtreme.Web/Controllers/AddMaterialController.cs
@@ -1,6 +1,38 @@
-﻿namespace ScrumExtreme.Web.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using ScrumExtreme.Application.Interfaces;
+using ScrumExtreme.Domain.Entities;
+using ScrumExtreme.Web.Models;
+
+public class AddMaterialController : Controller
 {
-    public class AddMaterialController
+    private readonly IMaterialService _materialService;
+
+    public AddMaterialController(IMaterialService materialService)
     {
+        _materialService = materialService;
+    }
+
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return View(new CreateMaterialViewModel());
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Index(CreateMaterialViewModel model)
+    {
+        if (!ModelState.IsValid)
+            return View("Index", model);
+
+        var material = new Material
+        {
+            Name = model.Name,
+            Price = model.Price
+        };
+
+        await _materialService.CreateMaterialAsync(material);
+
+        TempData["Success"] = "Material tillagt!";
+        return RedirectToAction(nameof(Index));
     }
 }
