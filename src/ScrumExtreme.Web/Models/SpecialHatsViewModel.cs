99856bfc3e33ab77f327e005790004affd45d7aa
Nordtess <nordtessj@gmail.com>
2026-04-22 16:42:42 +0200

    x

--- src/ScrumExtreme.Web/Models/SpecialHatsViewModel.cs
+++ src/ScrumExtreme.Web/Models/SpecialHatsViewModel.cs
@@ -1,25 +1,27 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.Collections.Generic;
+using ScrumExtreme.Domain.Entities;
 
 namespace ScrumExtreme.Web.Models
 {
     public class SpecialHatsViewModel
     {
-        [Required(ErrorMessage = "Namn är obligatoriskt.")]
-        public string Name { get; set; } = string.Empty;
-
-        [MinLength(1, ErrorMessage = "Minst en storlek måste väljas.")]
-        public List<string> Sizes { get; set; } = new();
+        [Required(ErrorMessage = "En storlek måste väljas.")]
+        public string Size { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Pris är obligatoriskt.")]
         public double Price { get; set; }
-
-        [Required(ErrorMessage = "Materiallista är obligatoriskt.")]
-        public string MaterialList { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Beskrivning är obligatoriskt.")]
         public string Description { get; set; } = string.Empty;
 
         public string CustomerId { get; set; } = string.Empty;
+
+        public List<string> SelectedMaterials { get; set; } = new();
+        public List<string> SelectedItems { get; set; } = new();
+
+        // Populated in GET for rendering checkboxes
+        public List<Material> AvailableMaterials { get; set; } = new();
+        public List<Item> AvailableItems { get; set; } = new();
     }
 }
