f1b637e92ab4a23e8519e8289ab92db069d406d3
adriangranstrom <adrianfran7g@gmail.com>
2026-04-27 15:58:37 +0200

    Visa statistik

--- src/ScrumExtreme.Web/Controllers/SalesStatisticsController.cs
+++ src/ScrumExtreme.Web/Controllers/SalesStatisticsController.cs
@@ -1,13 +1,31 @@
 using Microsoft.AspNetCore.Mvc;
+using ScrumExtreme.Application.Interfaces;
 
 namespace ScrumExtreme.Web.Controllers;
 
 [Route("Statistics")]
 public class SalesStatisticsController : Controller
 {
-    [HttpGet("")]
-    public IActionResult Index()
+    private readonly IOrderService _orderService;
+
+    public SalesStatisticsController(IOrderService orderService)
     {
+        _orderService = orderService;
+    }
+
+    [HttpGet("")]
+    public async Task<IActionResult> Index()
+    {
+        var orders = await _orderService.GetAllOrdersAsync();
+
+        var totalSales = orders.Sum(o => o.TotalAmount);
+        var totalOrders = orders.Count();
+
+        ViewBag.TotalSales = totalSales;
+        ViewBag.TotalOrders = totalOrders;
+        ViewBag.Orders = orders;
+
         return View();
     }
 }
+
